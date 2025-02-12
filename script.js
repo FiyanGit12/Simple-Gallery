479aaf1055fd539cd3008025a27f31aed3b60456
FiyanGit12 <alfiyanyan8@gmail.com>
2025-02-12 20:58:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typing = new Typed(".typing", {
+    strings: ["Everyone", "Aku Pemula", "I am Piyan"],  
+    typeSpeed: 100,  // waktu ketikan
+    backSpeed: 50,   // delay text yang di hapus
+    backDelay: 1000, // buat delay
+    loop: true       
+});
